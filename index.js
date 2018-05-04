7147344313083c3b07e7002724e520881cc79c8f
gu sheng <605661239@qq.com>
2018-05-04 18:01:46 +0800

    update

---
+++ index.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017')
+mongoose.connection.once('open', _ => console.log(mongoose.connection.name))
+
+const children = new mongoose.Schema({
+  age: Number,
+  name: String
+})
+
+const Schema1 = new mongoose.Schema({
+  age: Number,
+  name: String,
+  text: String,
+  arr: Array,
+  // children,
+})
+
+
+const User = mongoose.model('test', Schema1)
+User.update({}, {$push:{arr: 'fgeer3'}}).then(console.log)
+User.find({arr: {$in:['fgeer3']}}).then(console.log)
+
+
+// User.create({
+//   age: 22,
+//   name: 'gss',
+//   arr: [1]
+// })
+
+//   .then(res => console.log('ok', res))
+//   .catch(console.error)
