b498a2a6bc936e1b99e7b84a10c5e30a6391c930
gu sheng <605661239@qq.com>
2018-05-08 18:25:21 +0800

    update

--- text.js
+++ text.js
@@ -1,31 +1,16 @@
-const mongoose = require('mongoose')
-  , Schema = mongoose.Schema
+const jwt = require('jsonwebtoken')
 
-var PersonSchema = new Schema({
-  name: String,
-  band: String
-});
+var token = jwt.sign({ foo: 'bar' }, 'ff', {
+  expiresIn: 2 // s作为单位
+})
 
-var BandSchema = new Schema({
-  name: String
-});
-BandSchema.virtual('members', {
-  ref: 'Person', // The model to use
-  localField: 'name', // Find people where `localField`
-  foreignField: 'band', // is equal to `foreignField`
-  // If `justOne` is true, 'members' will be a single doc as opposed to
-  // an array. `justOne` is false by default.
-  justOne: false
-});
+let expiresIn
 
-var Person = mongoose.model('Person', PersonSchema);
-var Band = mongoose.model('Band', BandSchema);
-
-// Person.create({ name: 'gs', band: 'gg'})
-// Band.create({name: ''})
-/**
- * Suppose you have 2 bands: "Guns N' Roses" and "Motley Crue"
- * And 4 people: "Axl Rose" and "Slash" with "Guns N' Roses", and
- * "Vince Neil" and "Nikki Sixx" with "Motley Crue"
- */
-Band.find({}).populate('members').then(console.log)
+setTimeout(() => {
+  var token2 = jwt.sign({ foo: 'bar' }, 'ff', {
+    // expiresIn: 2 // s作为单位
+    expiresIn
+  })
+  
+  console.log(token2 === token)
+},3000);
